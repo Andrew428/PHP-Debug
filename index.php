cd8e27c9d37bb06c25704c9c7c9a868c229a1381
Andrew <andrew@andrewvc.net>
2021-04-29 16:38:54 -0500

    PHP-Debug
    Debug Breakpoints for PHP app

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+$var1 = "This is the 1 line";
+$var2 = "This is the 2 line";
+flush();
+$var3 = "This is the 3 line";
+$var4 = "This is the 4 line";
+$var5 = "This is the 5 line";
+$var6 = "This is the 6 line";
+
+//$var1 getcwd();
+phpinfo();
+
+?>
